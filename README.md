b7357a906e12e47b9d8ccf41bdab4741c17c6416
sichirsch <johannes.debler@gmail.com>
2019-11-18 21:24:07 +0800

    testing plotly

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # contiGC
 determine per contig / scaffold GC content
+
+# Requirements
+- numpy
+- plotly
+- requests
+- psutil
+
+# To Do
+- [x] import contigs fasta
+- [x] calculate per contig GC content and put into a list
+- [x] sort list by gc content
+- [ ] plot gc content as histogram (plotly)
+- [ ] option to export contigs within a GC range
---
+++ contiGC.py
@@ -0,0 +1,55 @@
+import csv
+import os
+from pathlib import Path
+import argparse
+import plotly
+import plotly.graph_objects as go
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='path to a fasta file containing contigs or scaffolds')
+args = parser.parse_args()
+
+if args.input:
+    input_path = Path(args.input)
+else:
+    #print('No input file provided')
+    input_path = Path('test.fasta')
+    #raise SystemExit
+
+gccontent ={}
+frequencies = []
+
+with open(input_path) as file:
+    input = file.read().splitlines()
+    for line in input:
+        if not line.strip(): continue
+        if line[0] == '>':
+            name = line[1:]
+            gccontent[name] = ''
+        else:
+            gc = 0
+            at = 0
+            for base in line:
+                if base.lower() == 'a':
+                    at += 1
+                if base.lower() == 't':
+                    at += 1
+                if base.lower() == 'c':
+                    gc += 1
+                if base.lower() == 'g':
+                    gc += 1
+            total = gc + at
+
+            gccontent[name] = 100/total*gc
+
+for key, value in sorted(gccontent.items(), key=lambda item: item[1]):
+    print("%s: %s" % (key, value))
+    frequencies.append(value)
+    
+lowest = frequencies[0]
+highest = frequencies[-1]
+spread = highest - lowest
+
+
+fig = go.Figure(data=go.Histogram(frequencies))
